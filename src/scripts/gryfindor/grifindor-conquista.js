a70661b6576f1033ed50ceb7b1bf9c8046e01f2f
PauloCesardev99 <paulocesarbritocamilo@gmail.com>
2023-12-10 17:11:49 -0300

    add mediaqueries acheiment ++

--- src/scripts/gryfindor/grifindor-conquista.js
+++ src/scripts/gryfindor/grifindor-conquista.js
@@ -5,16 +5,26 @@ const acordion = [...window.document.querySelectorAll(".acordion > h3")]
 const about = [...window.document.querySelectorAll(".acordion > p")]
 console.log(about)
 
+
+
+
 acordion.map((e,i)=>{
 
     e.addEventListener("click", ()=>{
 
         if(i==0){
             about[0].classList.toggle("desbloquear-about")
+            about[1].classList.remove("desbloquear-about")
+            about[2].classList.remove("desbloquear-about")
         } else if(i==1){
             about[1].classList.toggle("desbloquear-about")
+            about[0].classList.remove("desbloquear-about")
+            about[2].classList.remove("desbloquear-about")
+
         } else{
             about[2].classList.toggle("desbloquear-about")
+            about[0].classList.remove("desbloquear-about")
+            about[1].classList.remove("desbloquear-about")
         }
     })
 })
