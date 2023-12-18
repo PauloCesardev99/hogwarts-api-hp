50d3180daa888270834ee0238ce673607287ae75
PauloCesardev99 <paulocesarbritocamilo@gmail.com>
2023-12-18 13:48:23 -0300

    houses main add

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const casas = [...window.document.querySelectorAll(".casa > a > img")]
 
 
+
+
+casas.map((e,i)=>{
+
+    e.addEventListener("click", ()=>{
+
+        if(i!=0){
+            window.alert('Em Obras')
+        }
+
+    })
+})
